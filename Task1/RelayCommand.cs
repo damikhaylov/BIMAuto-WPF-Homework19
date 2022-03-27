156be873fa53a87d095e9490f19abee510236bec
Denis Mikhaylov <dnmh@yandex.ru>
2022-03-27 21:01:36 +0700

    Добавьте файлы проекта.

---
+++ Task1/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Task1
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<Object> execute;
+        private readonly Func<Object, bool> canExecute;
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<Object> Execute, Func<object, bool> CanExecute = null)
+        {
+            this.execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            this.canExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => execute(parameter);
+
+    }
+}
---
+++ Task1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Task1.Models;
+
+namespace Task1.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double circleLength;
+
+        public double CircleLength
+        {
+            get => circleLength;
+            set
+            {
+                circleLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CalcCommand { get; }
+        private void OnCalcCommandExecute(object p)
+        {
+            CircleLength = Geom.GetCircleLengthByRadius(Radius);
+        }
+        private bool CanCalcCommandExecuted(object p)
+        {
+            if (Radius > 0)
+            {
+                return true;
+            }
+            else
+            {
+                CircleLength = 0;
+                return false;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            CalcCommand = new RelayCommand(OnCalcCommandExecute, CanCalcCommandExecuted);
+        }
+    }
+}
